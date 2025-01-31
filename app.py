b6d8a484a0b83b55470efdab40c8e2952f0aa734
kimdongheecom <dongheekim@gmail.com>
2025-01-31 18:09:12 +0900

    폼 태그 작성 250131 18:00

--- app.py
+++ app.py
@@ -2,6 +2,11 @@ from flask import Flask, render_template
 app = Flask(__name__)
 
 @app.route('/')
+def login():
+
+   return render_template("auth/login.html")
+
+@app.route('/home')
 def home():
    #print("This is Home! 헬로우 월드 5")
    #return 'This is Home! 헬로우 월드 5'
@@ -65,8 +70,7 @@ def manufacture_esg_reporter():
 
    return render_template("esg/esg_analytics/manufacture_esg_reporter.html")
 
-if __name__ == '__main__':  
-   app.run('0.0.0.0',port=5000,debug=True)
+
 
 
 
@@ -89,5 +93,4 @@ def retail_finance_auto():
 if __name__ == '__main__':  
    app.run('0.0.0.0',port=5000,debug=True)
 
-
-
+app.config['TEMPLATES_AUTO_RELOAD'] = True
